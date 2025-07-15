8c51bf63515eb28afc44185196b5285febc624f6
Marcinlolik <marcin_lolik@hotmail.com>
2025-07-15 20:56:50 +0200

    Delete znajdzopiekunke-final directory

--- znajdzopiekunke-final/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Witamy na znajdzopiekunke.pl</h1>
-      <p>Profesjonalne połączenie opiekunek z osobami potrzebującymi wsparcia.</p>
-    </div>
-  );
-}
