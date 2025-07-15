55e29c7b88d12145ca6959c5af8335b7f19f8ea0
Marcinlolik <marcin_lolik@hotmail.com>
2025-07-15 20:51:13 +0200

    Add files via upload

---
+++ znajdzopiekunke-final/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Witamy na znajdzopiekunke.pl</h1>
+      <p>Profesjonalne połączenie opiekunek z osobami potrzebującymi wsparcia.</p>
+    </div>
+  );
+}
